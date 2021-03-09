9896c780ed7bac5587c22b7a3128c7408f439ed6
Nuno Courela <courela@gmail.com>
2021-03-09 22:20:33 +0000

    Unit test for #eval

--- UnitTests/DynamicPropertiesTests.cs
+++ UnitTests/DynamicPropertiesTests.cs
@@ -42,5 +42,20 @@ namespace JUST.UnitTests
 
             Assert.AreEqual("{\"root\":[{\"name1\":\"Jim\",\"number1\":\"0123-4567-8888\"},{\"name2\":\"John\",\"number2\":\"0134523-4567-8910\"}]}", result);
         }
+
+        [Test]
+        public void TypedValuesInsideEval()
+        {
+            const string input = "{ \"a\": [{ \"name\": \"prop1\", \"val\": 1 }, { \"name\": \"prop2\", \"val\": 2 }] }";
+            string transformer = "{ \"a\": { \"#loop($.a)\": { \"#eval(#currentvalueatpath($.name))\": \"#currentvalueatpath($.val)\" } } }";
+
+            var context = new JUSTContext
+            {
+                EvaluationMode = EvaluationMode.Strict
+            };
+            var result = new JsonTransformer(context).Transform(transformer, input);
+
+            Assert.AreEqual("{\"a\":[{\"prop1\":1},{\"prop2\":2}]}", result);
+        }
     }
 }
